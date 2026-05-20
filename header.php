67386de391f3038f007ac624f7aa1688dd5d45ed
“islamadel00” <eslam.adel01999@gmail.com>
2026-05-20 22:05:33 +0300

    perf: add preconnect for Google Fonts

--- header.php
+++ header.php
@@ -33,6 +33,8 @@ tailwind.config = {
   }
 }
 </script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@300;400;500;700;900&display=swap" rel="stylesheet"/>
 <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet"/>
 <style>
